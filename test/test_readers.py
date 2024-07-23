d510d9ac404834cbdb077bb63349ca67892c0826
krzysztof-kukawski <krzysztof.kukawski.ln@gmail.com>
2024-07-23 21:06:45 +0200

    fixed path in test_readers

--- test/test_readers.py
+++ test/test_readers.py
@@ -4,7 +4,7 @@ from readers import TxtFileReader
 
 class TxtFileReaderTest(unittest.TestCase):
     def test_read(self):
-        test_input_path = os.path.dirname(__file__) + r"\data\test_input.txt"
+        test_input_path = os.path.dirname(__file__) + "/data/test_input.txt"
         
         expected = [4, 8, 9, 0, 12, 1, 4, 2, 12, 12, 4, 4, 8, 11, 12, 0]
         actual = TxtFileReader.read(test_input_path)
