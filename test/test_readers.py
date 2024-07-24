b13ce38c8e8d18ae9cc617284c37db889a095d0f
krzysztof-kukawski <krzysztof.kukawski.ln@gmail.com>
2024-07-24 17:00:18 +0200

    changed path so it will work on windows and unix

--- test/test_readers.py
+++ test/test_readers.py
@@ -1,10 +1,11 @@
 import unittest
 import os
-from readers import TxtFileReader
+from src.readers import TxtFileReader
 
 class TxtFileReaderTest(unittest.TestCase):
     def test_read(self):
-        test_input_path = os.path.dirname(__file__) + "/data/test_input.txt"
+        directory = os.path.dirname(__file__) 
+        test_input_path = os.path.join(directory, 'data', 'test_input.txt')
         
         expected = [4, 8, 9, 0, 12, 1, 4, 2, 12, 12, 4, 4, 8, 11, 12, 0]
         actual = TxtFileReader.read(test_input_path)
